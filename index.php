5456a77bb59fa4c518398018f6d224cffd4ffd00
Robson Ricardo <robsonrg@gmail.com>
2022-02-24 13:59:24 +0000

    ajustes

--- index.php
+++ index.php
@@ -1,21 +1,61 @@
+<h1>POC - Gravar e ler metadados em arquivos PDFs</h1>
+
+<p>Descrevo 2 casos que estou com problemas e na sequencia tem algumas POCs e scripts.</p>
+
+<table border="1">
+<tr>
+    <td width="50%" valign="top">
+        <h3>Caso 1</h3>
+        <ul>
+            <li><img src="sucesso.png" /> Consigo criar os metadados padrões (creator, author, title, subject, keywords) e também com itens adicionais, usando XMP</li>
+            <li><img src="erro.png" /> Não consigo ler (depois de gerado o PDF)</li>
+        </ul>
+        <img src="caso1.png" />
+    </td>
+    <td width="50%" valign="top">
+        <h3>Caso 2</h3>
+        <ul>
+            <li><img src="erro.png" /> Não consigo criar o PDF com metadados customizados (que não são via XMP)</li>
+            <li><img src="sucesso.png" /> Consigo ler esses metadados</li>
+        </ul>
+
+        <img src="caso2.png" />
+    </td>
+</tr>
+</table>
+
+
+
+
+<hr />
+
 <p>Cria PDF com metadados padrões (Author, Title...) e personalizados (additional XMP)</p>
 <ul>
-    <li>
-        <a href="pdf_create_tcpdf.php">pdf_create_tcpdf</a>
-    </li>
+    <li>POC - <a href="pdf_create_tcpdf.php">pdf_create_tcpdf</a></li>
 </ul>
-<p>
-    Exif Viewer - file type analysis, meta, file information<br />
-    - <a href="https://exifviewer.herokuapp.com/">https://exifviewer.herokuapp.com/</a><br />
-    - <a href="https://www.metadata2go.com/">https://www.metadata2go.com/</a>
-</p>
+
 <hr />
+
+Faz a "leitura" dos PDFs para encontrar os metadata e outras informações
+
 <ul>
-    <li>
-        Ler
-        <ul>
-            <li><a href="pdf_read_pdfparser.php">pdf_read_pdfparser</a></li>
-            <li><a href="pdf_read_phpexiftool.php">pdf_read_phpexiftool</a></li>
-        </ul>
-    </li>
+    <li>POC - <a href="pdf_read_pdfparser.php">pdf_read_pdfparser</a></li>
+    <li>POC - <a href="pdf_read_phpexiftool.php">pdf_read_phpexiftool</a></li>
+</ul>
+
+Ferramentas online
+<p>
+    - <a href="https://exifviewer.herokuapp.com/">Exif Viewer</a><br />
+    - <a href="https://www.metadata2go.com/">Metadata2GO</a>
+</p>
+
+<hr />
+
+<p>Libs conhecidas e avaliei</p>
+<ul>
+    <li><a href="https://github.com/tecnickcom/TCPDF">TCPDF</a></li>
+    <li><a href="https://github.com/Setasign/FPDI">FPDI - Free PDF Document Importer</a></li>
+    <li><a href="http://www.fpdf.org/">FPDF</a></li>
+    <li><a href="https://github.com/smalot/pdfparser">PdfParser</a></li>
+    <li><a href="https://github.com/alchemy-fr/PHPExiftool">PHP-Exiftool</a></li>
 </ul>
